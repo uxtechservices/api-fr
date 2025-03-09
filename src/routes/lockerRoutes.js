a595bbcf1db34c047d3074dbd3ce5c2a61466f8a
uxtechservices <josephlopezf98@gmail.com>
2025-03-09 18:02:31 -0500

    Update lockerRoutes.js

--- src/routes/lockerRoutes.js
+++ src/routes/lockerRoutes.js
@@ -1,21 +1,16 @@
 import express from 'express';
 import { initDB, getLocker, getAll, createNewLocker, updateExistingLocker, deleteExistingLocker, assignExistingLocker, unassignExistingLocker } from '../controllers/lockerController.js';
-import authenticateToken from '../middlewares/authMiddleware.js';
-import { validateCreateLocker } from '../middlewares/validationMiddleware.js';
 
 const router = express.Router();
 
 router.get('/init', initDB);
 
-// Example of applying authentication middleware to specific routes
-router.get('/:lockerId', authenticateToken, getLocker);
-router.get('/', authenticateToken, getAll);
-
-// Example of applying validation middleware before creating a locker
-router.post('/', authenticateToken, validateCreateLocker, createNewLocker);
-router.put('/:lockerId', authenticateToken, updateExistingLocker);
-router.delete('/:lockerId', authenticateToken, deleteExistingLocker);
-router.post('/:lockerId/assign', authenticateToken, assignExistingLocker);
-router.post('/:lockerId/unassign', authenticateToken, unassignExistingLocker);
+router.get('/:lockerId', getLocker);
+router.get('/', getAll);
+router.post('/', createNewLocker);
+router.put('/:lockerId', updateExistingLocker);
+router.delete('/:lockerId', deleteExistingLocker);
+router.post('/:lockerId/assign', assignExistingLocker);
+router.post('/:lockerId/unassign', unassignExistingLocker);
 
 export default router;
